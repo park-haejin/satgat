f9cf92a6b32e49c1c0ea0063fe9f47340db71c22
park-haejin <byhaejin.02@gmail.com>
2016-12-23 11:19:38 +0900

    deal view

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -7,6 +7,11 @@ var app = {
             //탑베너 : 종료
             $('.closeTopBannerBtn').bind("click",function(e){
                 $('.Wrap').addClass('disableTopBnr');
+                if (  $('#sort-menu') ) { // 페이지가 딜화면이라면
+                    $('#sort-menu').scrollToFixed( {
+                        marginTop: 46
+                    });
+                }
             });
             //로그인 : 바로가기
             $('.loginBtn').bind("click",function(e){
@@ -19,6 +24,11 @@ var app = {
             //알림창 토글
             $('.userMailBtn').bind("click",function(e){
                 $('.alarm').toggleClass('on')
+            });
+
+            // 모바일버전 탑메뉴 토글
+            $('.mb-topmenu .listBtn, .closeSideMenuBtn').bind("click",function(e){
+                $('.mobile-menu').toggleClass("active")
             });
         }
     }
